79c39a197fab3e75ecf7a13dfeaae26c9d3f2841
nav4 <nav4@umbc.edu>
2017-04-01 18:55:05 -0400

    timer
    changed from float to double

--- driver.cpp
+++ driver.cpp
@@ -15,7 +15,6 @@ int main()
   // space-efficient to use the default, but then the
   // numbers will print in binary.
   	PosInt::setBase(16, 3);
-
 	
   // x = 2^128
   	PosInt x(2);
@@ -33,25 +32,32 @@ int main()
   	//cout << "z = rand(x) = " << z << endl;
   	cout << "z  = " << z << endl;
 
-	clock_t begin = clock();
+	time_t begin;
   // z = z^2
+
+	time(&begin);
   	z.mul(z);
+
+	time_t end;
+	time(&end);
+
   	cout << "z^2 = " << z << endl;
-	clock_t end = clock();
-	float elapsed_sec = (float(end - begin) / CLOCKS_PER_SEC);
-	cout << "Time " << elapsed_sec << endl;	
+	double diff;
+	diff = difftime(end, begin);
+	double elapsed_sec = (1000 * double(end - begin) / CLOCKS_PER_SEC);
+	cout << "Time elapsed: " << elapsed_sec << endl;	
+	cout << "Execution time: " << diff << endl;
 	
   // z.set(10000000);
   	cout << "a =  " << a << endl;
-
 
 	clock_t kbegin = clock();
 
   	a.fastMul(a);
 
 	clock_t kend = clock();
-	float kelapsed_sec = (float(kend - kbegin) / CLOCKS_PER_SEC);
-	cout << "Time " << kelapsed_sec << endl;
+	double kelapsed_sec = (1000 * double(kend - kbegin) / CLOCKS_PER_SEC);
+	cout << "Time elapsed: " << kelapsed_sec << endl;
 	
   	cout << "Fast mul a^2 = " << a << endl;
 	
@@ -59,5 +65,6 @@ int main()
   	z.mod(x);
 
  	cout << "z^2 mod x = " << z << endl;
+
 }
   
